0eb130436fdccf527ab9cefbc86e20b31c215b79
Mille <mille.bostrom@testura.net>
2017-01-30 19:49:45 +0100

    Fixed tests

--- src/Testura.Android.Tests/Integration/Device/UiAutomator/Server/UiAutomatorServerTests.cs
+++ src/Testura.Android.Tests/Integration/Device/UiAutomator/Server/UiAutomatorServerTests.cs
@@ -15,7 +15,7 @@ namespace Testura.Android.Tests.Integration.Device.UiAutomator.Server
         [SetUp]
         public void SetUp()
         {
-            _server = new UiAutomatorServer(new Terminal(new DeviceConfiguration()), 9008, true);
+            _server = new UiAutomatorServer(new Terminal(new DeviceConfiguration()), 9008);
         }
 
         [Test]
