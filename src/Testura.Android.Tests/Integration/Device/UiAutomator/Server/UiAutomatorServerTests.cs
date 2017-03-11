4848a9eb8ebdb1ed0cf315c4d44221fff5debb9b
Mille <mille.bostrom@testura.net>
2017-03-11 19:55:03 +0100

    Added correct tag to test

--- src/Testura.Android.Tests/Integration/Device/UiAutomator/Server/UiAutomatorServerTests.cs
+++ src/Testura.Android.Tests/Integration/Device/UiAutomator/Server/UiAutomatorServerTests.cs
@@ -1,7 +1,5 @@
 ﻿using NUnit.Framework;
-using Testura.Android.Device;
 using Testura.Android.Device.Configurations;
-using Testura.Android.Device.Ui.Search;
 using Testura.Android.Device.Ui.Server;
 using Testura.Android.Util.Terminal;
 using Assert = NUnit.Framework.Assert;
@@ -9,6 +7,7 @@ using Assert = NUnit.Framework.Assert;
 namespace Testura.Android.Tests.Integration.Device.UiAutomator.Server
 {
     [TestFixture]
+    [Ignore("Integration")]
     public class UiAutomatorServerTests
     {
         private UiAutomatorServer _server;
@@ -28,13 +27,10 @@ namespace Testura.Android.Tests.Integration.Device.UiAutomator.Server
         [Test]
         public void UiAUtomatorServer_WhenDumpingUi_ShouldGetContentBack()
         {
-            //_server.Start();
-            //var ui = _server.DumpUi();
-            //Assert.IsNotNull(ui);
-            //Assert.IsNotEmpty(ui);
-
-            var device = new AndroidDevice(new DeviceConfiguration());
-            device.Ui.CreateUiObject(With.ResourceId("android:id/search_src_text")).InputText("171");
+            _server.Start();
+            var ui = _server.DumpUi();
+            Assert.IsNotNull(ui);
+            Assert.IsNotEmpty(ui);
         }
     }
 }
