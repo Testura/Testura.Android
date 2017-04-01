ae1f0d01c17c8d5bab74712a57dd9ea0c23dfed2
Mille <mille.bostrom@testura.net>
2017-04-02 01:03:24 +0200

    Started on the click monkey

---
+++ src/Testura.Android/Util/Monkey/ClickMonkey.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Testura.Android.Device;
+using Testura.Android.Device.Services.Default;
+using Testura.Android.Device.Ui.Nodes.Data;
+using Testura.Android.Device.Ui.Search;
+
+namespace Testura.Android.Util.Monkey
+{
+    public class ClickMonkey
+    {
+        public Random _rnd;
+
+        public ClickMonkey()
+        {
+            _rnd = new Random();
+        }
+
+        public void Start(IAndroidDevice device, string package, string activity, IEnumerable<MonkeyCase> monkeyCases)
+        {
+            var d = new AndroidDevice();
+            d.Activity.Start(package, activity, false, false);
+
+            var o = device.Ui as UiService;
+            while (true)
+            {
+                var nodes = o.GetAllNodesOnScreen();
+
+                var selectedNode = nodes[_rnd.Next(0, nodes.Count)];
+
+                foreach (var monkeyCase in monkeyCases)
+                {
+                    if (MatchCase(monkeyCase.Withs, selectedNode))
+                    {
+                        var shouldStillClick = monkeyCase.Case(d, selectedNode);
+                        if (shouldStillClick)
+                        {
+                            device.Interaction.Tap(selectedNode);
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool MatchCase(IList<With> withs, Node node)
+        {
+            foreach (var with in withs)
+            {
+                if (!with.NodeSearch(node))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Testura.Android/Util/Monkey/MonkeyCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Testura.Android.Device;
+using Testura.Android.Device.Ui.Nodes.Data;
+using Testura.Android.Device.Ui.Search;
+
+namespace Testura.Android.Util.Monkey
+{
+    public class MonkeyCase
+    {
+        /// <summary>
+        /// Gets or sets the with for the special case node
+        /// </summary>
+        public IList<With> Withs { get; set; }
+
+        /// <summary>
+        /// Gets or sets the func that should be executed when we find the special case node. Returns
+        /// true if we should still click on the node, false if not.
+        /// </summary>
+        public Func<IAndroidDevice, Node, bool> Case { get; set; }
+    }
+}
