0264cff41353550c54ca063264ef2bab2f8314b8
Mille <mille.bostrom@testura.net>
2018-05-22 11:42:57 +0200

    Changed how we send http request

---
+++ src/Testura.Android/Util/Http/TimeoutHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testura.Android.Util.Http
+{
+    internal class TimeoutHandler : DelegatingHandler
+    {
+        public TimeoutHandler(TimeSpan defaultTimeout, HttpClientHandler innerHandler)
+        {
+            DefaultTimeout = defaultTimeout;
+            InnerHandler = innerHandler;
+        }
+
+        public TimeSpan DefaultTimeout { get; set; }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            using (var cts = GetCancellationTokenSource(request, cancellationToken))
+            {
+                try
+                {
+                    return await base.SendAsync(request, cts?.Token ?? cancellationToken);
+                }
+                catch (OperationCanceledException) when (!cancellationToken.IsCancellationRequested)
+                {
+                    throw new TimeoutException();
+                }
+            }
+        }
+
+        private CancellationTokenSource GetCancellationTokenSource(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var timeout = request.GetTimeout() ?? DefaultTimeout;
+            if (timeout == Timeout.InfiniteTimeSpan)
+            {
+                // No need to create a CTS if there's no timeout
+                return null;
+            }
+            else
+            {
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                cts.CancelAfter(timeout);
+                return cts;
+            }
+        }
+    }
+}
